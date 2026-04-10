1a03b6572909bb6c373935c5784b1e5d06520618
sarki012 <erik.sarkinen@gmail.com>
2026-04-10 16:07:09 -0500

    Ringtone Dings When RMS Crosses Threshold On Right Side. Showed John.

--- app/src/main/java/com/esark/framework/AndroidGraphics.java
+++ app/src/main/java/com/esark/framework/AndroidGraphics.java
@@ -110,7 +110,7 @@ public class AndroidGraphics extends AndroidGame implements Graphics {
     public void drawRedLine(int x, int y, int x2, int y2, int color) {
 
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(10);
+        paint.setStrokeWidth(5);
         canvas.drawLine(x, y, x2, y2, paint);
         return;
     }
