313e3cd77e92c3852d2e6e8875bf4f0ad047b037
sarki012 <erik.sarkinen@gmail.com>
2026-06-01 17:16:43 -0500

    AI Moved Handler Code to Executor Thread In ConnectedThread. Still Not Perfect.

--- app/src/main/java/com/esark/gasp/GameScreen.java
+++ app/src/main/java/com/esark/gasp/GameScreen.java
@@ -412,8 +412,8 @@ public class GameScreen extends Screen implements Input {
         if (!isReplaying) {
             // --- LIVE BLACK LINE ---
             xStart = 1600;
-            int xStep = 12;
-            int drawSkip = 20;
+            int xStep = 2;
+            int drawSkip = 1;
             for (int n = signalBufferLen - 1; n > drawSkip; n -= drawSkip) {
                 int y1 = (int) (screenCenterY - (A2DVal[n] - dataBaseline) * gain);
                 int y2 = (int) (screenCenterY - (A2DVal[n - drawSkip] - dataBaseline) * gain);
